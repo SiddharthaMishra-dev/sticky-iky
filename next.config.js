2a43d02240e9b010b92cbf68765a3cadd67e6c5d
kaioken <mailbox.siddharthamishra@gmail.com>
2024-09-27 00:51:12 +0530

    fix: manifest json

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  // disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
@@ -10,3 +10,4 @@ const withPWA = require("next-pwa")({
 const nextConfig = {};
 
 module.exports = withPWA(nextConfig);
+// module.exports = nextConfig;
